c035c8fed38bca725e88d0329048171b82f54db2
simtsit <symeon.tsitoglou@corp.Altegrity.com>
2018-05-21 15:36:45 +0200

    A few minor changes for testing git. Nothing important to declare.

--- src/app/person.model.ts
+++ src/app/person.model.ts
@@ -1,8 +1,11 @@
+// Setting the basic model (Person)
+
 export class Person {
 	constructor(
-	public firstName: string,
-	public lastName: string,
-	public email: string,
-	public city: string
+		public firstName: string, // for First Name
+		public lastName: string, // for Last Name
+		public email: string, // for Email
+		public city: string // for City  --> Selection List
+		// public radio --> for Radio Buttons --> Comming soon.
 	){}
 }
